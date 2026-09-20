8de1a3824ada6034231297f89ceda30398883182
Retroid_Pocket_Five <retroid.pocket.five+GitHub@gmail.com>
2026-09-20 17:18:44 +0100

    Delete build.js

--- build.js
+++
@@ -1,7 +0,0 @@
-const fs=require('fs');
-for(const n of ['cinejoy','cinewave']){
-  const src='src/'+n+'/index.js', out='providers/'+n+'.js';
-  if(!fs.existsSync(src)) throw new Error('Missing '+src);
-  fs.copyFileSync(src,out);
-  console.log('built '+out);
-}
